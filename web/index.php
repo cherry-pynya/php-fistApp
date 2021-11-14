fabdd0b588ba820bc9836893cc7b73a7cc57a4ca
chery-pynya <nikita.cherepnya@gmail.com>
2021-11-14 21:48:48 +0300

    name visible

--- web/index.php
+++ web/index.php
@@ -5,6 +5,8 @@ session_start();
 $link = "<a href='./exit.php'>exit</a>";
 
 if (array_key_exists('name', $_SESSION)) {
+    echo 'Your name is ' . $_SESSION["name"];
+    echo '<br />';
     echo $link;
 } else {
     include './page.php';
